0aff2085307bac9ee91badb7e1fdfc1987b72999
ysjung <ysjung@maninsoft.co.kr>
2011-11-15 00:07:43 +0900

    To pull

--- WebContent/js/sw/sw-util.js
+++ WebContent/js/sw/sw-util.js
@@ -15,3 +15,27 @@
  		return result;
  	}
  };
+ 
+	function setCookie() {
+		var id;
+		id = document.loginForm.j_username.value;
+		var todayDate = new Date();
+		todayDate.setDate(todayDate.getDate() + 365);			
+		document.cookie = "smartWorks=on; expires=" + todayDate.toGMTString()+ "; path=/" ; 
+		document.cookie = "smartWorksid="+ id +";  expires=" + todayDate.toGMTString()+ "; path=/" ; 
+	}
+	function unSetCookie() {
+		var todayDate = new Date();
+		todayDate.setDate(todayDate.getDate() + 365);
+		document.cookie = "smartWorks=off; expires=" + todayDate.toGMTString()+ "; path=/" ; 
+		document.cookie = "smartWorksid=;  expires=" + todayDate.toGMTString()+ "; path=/" ; 
+	}
+	function cookieVal(cookieName) {
+		thisCookie = document.cookie.split("; ");
+		for(i=0; i<thisCookie.length; i++){
+			if(cookieName == thisCookie[i].split("=")[0]) {
+				return thisCookie[i].split("=")[1];
+			}
+		}
+		return "x";
+	}
