299ff6fac58398332b196862e100ae141fb20f2b
ysjung <ysjung@maninsoft.co.kr>
2012-02-18 15:27:27 +0900

    To pull

--- src/net/smartworks/model/work/info/WorkCategoryInfo.java
+++ src/net/smartworks/model/work/info/WorkCategoryInfo.java
@@ -4,8 +4,17 @@ import net.smartworks.model.work.WorkCategory;
 
 public class WorkCategoryInfo extends WorkInfo {
 
+	private boolean running;
+	
+	public boolean isRunning() {
+		return running;
+	}
+	public void setRunning(boolean running) {
+		this.running = running;
+	}
 	public WorkCategoryInfo(){
 		super();
+		super.setType(WorkCategory.TYPE_CATEGORY);
 	}
 	public WorkCategoryInfo(String id, String name){
 		super(id, name, WorkCategory.TYPE_CATEGORY);
